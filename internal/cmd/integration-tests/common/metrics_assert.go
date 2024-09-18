54133361b0ba68c8fdffb748987af84f577b39fc
William Dumont <william.dumont@grafana.com>
2024-09-18 12:24:01 +0200

    test

--- internal/cmd/integration-tests/common/metrics_assert.go
+++ internal/cmd/integration-tests/common/metrics_assert.go
@@ -91,7 +91,6 @@ func AssertMetricsAvailable(t *testing.T, metrics []string, histogramMetrics []s
 func checkMissingMetrics(expectedMetrics []string, actualMetrics []Metric) []string {
 	metricSet := make(map[string]struct{}, len(actualMetrics))
 	for _, metric := range actualMetrics {
-		fmt.Println("Actual metric: ", metric.Name)
 		metricSet[metric.Name] = struct{}{}
 	}
 
